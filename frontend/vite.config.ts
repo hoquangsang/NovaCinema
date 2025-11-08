b02a3332da6cd8f2b1504f99c4836718acb79f36
23120409 <khacvuong2707@gmail.com>
2025-11-08 23:14:06 +0700

    feat(fe): initialize Vite + TypeScript + Tailwind v4 set up

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from "@tailwindcss/vite";
-
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwind(),
-  ],
-  resolve: { alias: { '@': '/src' } },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000', // backend NestJS
-    },
-  },
+  plugins: [react(), tailwindcss()],
 })
