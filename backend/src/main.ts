353b95399703274804c72ab59ad3a197de803852
Hồ Quang Sang <sangdinhban@gmail.com>
2025-11-17 22:15:25 +0700

    chore(be): add Swagger setup for API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document, { swaggerOptions: { persistAuthorization: true } });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
