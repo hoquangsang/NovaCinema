2101bb13328ad98284ca9e7d6763be262952567c
tranquocvy <quocvy23072005@gmail.com>
2025-11-22 22:10:11 +0700

    feat(router): add /movie/:id route for movie detail page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/layout/MainLayout';
-import HomePage from './pages/HomePage';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import HomePage from "./pages/HomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+import "./App.css";
 
 function App() {
   return (
@@ -10,15 +11,15 @@ function App() {
       <Routes>
         {/* Route cha "/" sẽ dùng MainLayout */}
         <Route path="/" element={<MainLayout />}>
-          
           {/* Khi truy cập "/", route con `index` sẽ được render */}
           {/* Đây chính là HomePage, nó sẽ chui vào <Outlet /> của MainLayout */}
           <Route index element={<HomePage />} />
-          
+
           {/* Thêm các trang khác ở đây sau này, ví dụ: */}
           {/* <Route path="login" element={<div>Login Page</div>} /> */}
           {/* <Route path="movie/:id" element={<div>Movie Detail Page</div>} /> */}
-
+          {/* Movie detail */}
+          <Route path="movie/:id" element={<MovieDetailPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
