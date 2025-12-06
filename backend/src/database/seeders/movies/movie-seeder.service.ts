68183c815d369c774f7dc24eee21fc71e559f8a6
Hồ Quang Sang <sangdinhban@gmail.com>
2025-12-06 23:16:01 +0700

    refactor(be): update movie seed data and seeder service

--- backend/src/database/seeders/movies/movie-seeder.service.ts
+++ backend/src/database/seeders/movies/movie-seeder.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Movie, MovieDocument } from 'src/modules/movies/schemas/movie.schema';
-import { MOVIES_MOCK } from './movie-seeder.data';
+import { UPCOMING_MOVIES_DATA, SHOWING_MOVIES_DATA } from './movie-seeder.data';
 
 @Injectable()
 export class MovieSeederService {
@@ -14,6 +14,7 @@ export class MovieSeederService {
   async seed() {
     await this.movieModel.deleteMany();
 
-    await this.movieModel.insertMany(MOVIES_MOCK);
+    await this.movieModel.insertMany(UPCOMING_MOVIES_DATA);
+    await this.movieModel.insertMany(SHOWING_MOVIES_DATA);
   }
 }
