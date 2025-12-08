d55408fb50024150014ee97d5818b698c49891a9
23120410 <quocvy23072005@gmail.com>
2025-12-08 12:24:50 +0700

    Revert "feat(backend): add domain layer with business models"

--- backend/src/domain/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Domain Layer Exports
- * This layer contains pure business logic and entities
- * No infrastructure or framework dependencies
- */
-
-export * from './models';
-export * from './interfaces';
--- backend/src/domain/interfaces/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Domain Interfaces Export
- */
-
-export * from './repositories.interface';
-export * from './services.interface';
