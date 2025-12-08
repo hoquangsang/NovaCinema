51bf2881e8cefc7fb079d75182b897205109f55f
23120410 <quocvy23072005@gmail.com>
2025-12-08 12:24:26 +0700

    Revert "feat(backend): add application layer with use cases"

--- backend/src/application/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Application Layer
- * Orchestrates domain models and implements use cases
- */
-
-// Use Cases
-export * from './use-cases/booking';
--- backend/src/application/use-cases/booking/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Use Case Exports: Booking
- */
-
-export * from './create-booking.use-case';
-export * from './confirm-booking.use-case';
-export * from './cancel-booking.use-case';
