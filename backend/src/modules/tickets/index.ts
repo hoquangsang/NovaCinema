da5454fdd7150a77bcc2ca8d72d7d81c50adbfcb
Hồ Quang Sang <sangdinhban@gmail.com>
2026-01-12 11:02:29 +0700

    chore(be): register modules and update dependencies

--- backend/src/modules/tickets/index.ts
+++
@@ -1,5 +0,0 @@
-export type { TicketDocument } from './schemas';
-
-export { Ticket, TicketSchema } from './schemas';
-export { TicketService } from './services';
-export { TicketsModule } from './tickets.module';
