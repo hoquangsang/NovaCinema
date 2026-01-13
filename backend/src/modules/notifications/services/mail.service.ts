774f65e68946644ee598b742038cc072dbf909c1
Hồ Quang Sang <sangdinhban@gmail.com>
2026-01-13 10:10:37 +0700

    refactor(be): migration from SendGrid to Resend and improve email templates

--- backend/src/modules/notifications/services/mail.service.ts
+++ backend/src/modules/notifications/services/mail.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import sgMail, { MailDataRequired } from '@sendgrid/mail';
+import { Resend } from 'resend';
 
 export interface SendMailOptions {
   to: string;
   subject: string;
   html: string;
+  text?: string;
 }
 
 @Injectable()
@@ -13,29 +14,33 @@ export class MailService {
   private readonly mailFrom: string;
   private readonly mailFromName: string;
   private readonly logger = new Logger(MailService.name);
+  private readonly resend: Resend;
 
   constructor(private readonly config: ConfigService) {
     this.mailFrom = this.config.getOrThrow('MAIL_FROM');
     this.mailFromName = this.config.get('MAIL_FROM_NAME') ?? 'NovaCinema';
-    sgMail.setApiKey(this.config.getOrThrow('SENDGRID_API_KEY'));
+
+    this.resend = new Resend(this.config.getOrThrow('RESEND_API_KEY'));
   }
 
   async send(options: SendMailOptions) {
-    const { html, subject, to: mailTo } = options;
-
-    const msg: MailDataRequired = {
+    const {
+      html,
+      subject,
       to: mailTo,
-      from: {
-        email: this.mailFrom,
-        name: this.mailFromName,
-      },
-      subject: subject,
-      html: html,
-    };
+      text = `${this.mailFromName} - ${subject}`,
+    } = options;
 
     try {
-      await sgMail.send(msg);
-      this.logger.log('[MAIL] Sent OK');
+      const response = await this.resend.emails.send({
+        to: mailTo,
+        from: `${this.mailFromName} <${this.mailFrom}>`,
+        subject,
+        html,
+        text,
+      });
+
+      this.logger.log('[MAIL] Sent OK', response);
       return true;
     } catch (err) {
       this.logger.error('[MAIL] Send FAILED');
